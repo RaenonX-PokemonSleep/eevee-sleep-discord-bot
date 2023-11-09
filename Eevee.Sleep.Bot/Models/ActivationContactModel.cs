1bf1196464e7044deb1bc9bd041feed6e821085b
RaenonX <raenonx0710@gmail.com>
2023-11-08 21:21:46 -0600

    Fixed `ActivationContactModel` causes crash if additional contact exists

--- Eevee.Sleep.Bot/Models/ActivationContactModel.cs
+++ Eevee.Sleep.Bot/Models/ActivationContactModel.cs
@@ -1,7 +1,9 @@
 ﻿using JetBrains.Annotations;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Eevee.Sleep.Bot.Models;
 
+[BsonIgnoreExtraElements]
 public record ActivationContactModel {
     [UsedImplicitly]
     public required string? Discord { get; init; }
