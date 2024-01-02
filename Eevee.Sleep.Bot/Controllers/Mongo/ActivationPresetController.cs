9dfa114241b66c19cf750907c24f3c4346424fe0
RaenonX <raenonx0710@gmail.com>
2024-01-01 21:09:46 -0600

    Fixed incorrect role ID tag checking impl

--- Eevee.Sleep.Bot/Controllers/Mongo/ActivationPresetController.cs
+++ Eevee.Sleep.Bot/Controllers/Mongo/ActivationPresetController.cs
@@ -6,10 +6,7 @@ namespace Eevee.Sleep.Bot.Controllers.Mongo;
 public static class ActivationPresetController {
     public static HashSet<ulong> GetTaggedRoleIds() {
         return MongoConst.AuthActivationPresetCollection
-            .Find(x =>
-                x.Source == GlobalConst.SubscriptionSourceOfDiscord ||
-                x.Source == GlobalConst.SubscriptionSourceOfPatreon
-            )
+            .Find(x => x.Source == GlobalConst.SubscriptionSourceOfDiscord)
             .ToEnumerable()
             .Select(x => ulong.Parse(x.Tag))
             .ToHashSet();
