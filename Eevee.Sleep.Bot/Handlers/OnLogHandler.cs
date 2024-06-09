ac74671d486f8757aba08b7a720d32c84fa48a3c
RaenonX <raenonx0710@gmail.com>
2024-06-09 08:03:25 -0500

    Added Discord timeout exception to error report exclusion
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- Eevee.Sleep.Bot/Handlers/OnLogHandler.cs
+++ Eevee.Sleep.Bot/Handlers/OnLogHandler.cs
@@ -11,7 +11,10 @@ public static class OnLogHandler {
     public static async Task OnLogAsync(DiscordSocketClient client, LogMessage message) {
         if (
             message.Exception is not null &&
-            message.Exception is not GatewayReconnectException &
+            // Discord bot auto reconnect
+            message.Exception is not GatewayReconnectException &&
+            // Discord timeout
+            message.Exception.Message != "Cannot respond to an interaction after 3 seconds!" &&
             message.Exception.Message != "WebSocket connection was closed"
         ) {
             await client.SendMessageInAdminAlertChannel(embed: DiscordMessageMaker.MakeErrorFromLog(message));
