aabebdcbd6a1d61b3452af26676432b764cc63ac
ykono <yfukyd@gmail.com>
2024-07-02 00:10:42 +0900

    Fixed log output format

--- Eevee.Sleep.Bot/Extensions/DictionaryExtension.cs
+++ Eevee.Sleep.Bot/Extensions/DictionaryExtension.cs
@@ -1,9 +1,18 @@
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 namespace Eevee.Sleep.Bot.Extensions;
 
 public static class DictionaryExtensions {
+    private static readonly JsonSerializerOptions Options = new() {
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+        WriteIndented = true
+    };
+    
     public static string ToJsonString<K, V>(this IDictionary<K, V?> dict) {
-        return JsonSerializer.Serialize(dict);
+        return JsonSerializer.Serialize(
+            value: dict,
+            options: Options
+        );
     }
 }
